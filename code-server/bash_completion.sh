0df62c0d25fb281b42b277410cecfe62da7acfd8
Yannig Perré <yannig.perre@octo.com>
2025-02-13 14:16:45 +0100

    feat(code-server) autocompletion git/docker/kube

--- code-server/bash_completion.sh
+++ code-server/bash_completion.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
+source /usr/share/bash-completion/completions/git
+source <(docker completion bash)
+source <(kubectl completion bash)
+alias k=kubectl
+complete -o default -F __start_kubectl k
+
 source /usr/local/bin/kube-ps1.sh
 export PS1='[${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] $(kube_ps1)] $ '

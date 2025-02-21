f451becb2a2c76c78ca40ec685bebb1322d2fc17
Joseph Page <josephpage@users.noreply.github.com>
2025-02-21 11:14:33 +0100

    feat(download-api): new project download-api

---
+++ download-api/go.mod
@@ -0,0 +1,10 @@
+module download-api
+
+go 1.18
+
+require (
+	github.com/knadh/koanf v1.5.0 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+)
---
+++ download-api/internal/config/config.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"github.com/knadh/koanf"
+	"github.com/knadh/koanf/providers/confmap"
+	"github.com/knadh/koanf/providers/env"
+)
+
+var K = koanf.New(".")
+
+var defaultConfig = map[string]interface{}{
+	"HOST":          "0.0.0.0",
+	"PORT":          "8080",
+	"FOLDER_PATH":   "/default/path",
+	"ZIP_FILE_NAME": "archive.zip",
+}
+
+func LoadConfig() {
+	// Load default configuration from the defaultConfig map
+	K.Load(confmap.Provider(defaultConfig, "."), nil)
+
+	// Override with environment variables
+	K.Load(env.Provider("", ".", func(s string) string {
+		return s
+	}), nil)
+}
